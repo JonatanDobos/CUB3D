de3c85c5f553b9ab5eab0ba288b82af7c2f6751a
JonatanDobos <jonidobos@gmail.com>
2024-09-09 17:40:41 +0200

    first

---
+++ cub.h
@@ -0,0 +1,14 @@
+#ifndef CUB_H
+# define CUB_H
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <limits.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdbool.h>
+
+
+#endif
